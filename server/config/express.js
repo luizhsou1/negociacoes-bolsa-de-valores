fac4ebf8f03b7ada10aed00309bb49d6af5bda41
Luiz Henrique <luihsou1@gmail.com>
2020-08-01 15:33:29 -0300

    projeto final de negociacoes

--- server/config/express.js
+++ server/config/express.js
@@ -11,11 +11,11 @@ console.log(app.get('clientPath'));
 app.use(express.static(app.get('clientPath')));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 routes(app);
